e18ffae1fae8ae2d161c6df122e5c8e31512815f
Ed Bassett <ed@devsketchpad.com>
2012-04-20 00:49:37 +1000

    whitespace

--- codeigniter/application/views/ideas/view.php
+++ codeigniter/application/views/ideas/view.php
@@ -1,15 +1,15 @@
 <div class="page">
 	<span class="thumbnail"><a href="<?=$idea['url']?>"><img src="/uploads/<?php echo $idea['image']; ?>" alt="project thumbnail" /></a></span>
-    <div class="details">
-        <p><?=$idea['description']?></p>
+	<div class="details">
+		<p><?=$idea['description']?></p>
 		<?php if($idea['details']):?>
 		<h4>Details</h4>
-        <p><?=$idea['details']?></p>
+		<p><?=$idea['details']?></p>
 		<?php endif?>
 		<h4>Try it</h4>
-        <?=$idea['instructions']?>
+		<?=$idea['instructions']?>
 		<h4>Credits</h4>
-        <?=$idea['credits']?>
-	    <p><?=$idea['date']?></p>
-    </div>
+		<?=$idea['credits']?>
+		<p><?=$idea['date']?></p>
+	</div>
 </div>
