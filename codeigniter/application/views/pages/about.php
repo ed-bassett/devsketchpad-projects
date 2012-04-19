ab090628c05e7f589853c077c2acdf8b0b002f13
Ed Bassett <ed@devsketchpad.com>
2012-04-20 00:43:14 +1000

    updated about text

--- codeigniter/application/views/pages/about.php
+++ codeigniter/application/views/pages/about.php
@@ -1,3 +1,3 @@
 <div class="page">
-<p>This is a sketchpad for my web development projects and game development ideas. They are all in various stages of completeness and will likely have bugs.</p>
+<p>This is a sketchpad for my web development projects and game development ideas. They are all in various stages of completeness and will likely have bugs; the point is more to learn than to create perfection.</p>
 </div>
