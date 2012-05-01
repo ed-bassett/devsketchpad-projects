d1b656d3e376d1514d8a8a361f710d3465085333
Ed Bassett <ed@devsketchpad.com>
2012-05-01 23:54:15 +1000

    removed image link on idea page as it would've been confusing given it links to each idea from the gallery. Added button and styles instead

--- codeigniter/application/views/ideas/view.php
+++ codeigniter/application/views/ideas/view.php
@@ -1,8 +1,6 @@
 <div class="page">
 	<span class="thumbnail">
-		<?php if ($idea['url']):?><a href="<?=$idea['url']?>"><?endif?>
-			<img src="/uploads/<?=$idea['image']?>" alt="project thumbnail" />
-		<?php if ($idea['url']):?></a><?endif?>
+		<img src="/uploads/<?=$idea['image']?>" alt="project thumbnail" />
 	</span>
 	<?php if ( $this->tank_auth->is_logged_in() ): ?>
 	<span class="icons"><a href="/gallery/idea_edit/<?=$idea['id']?>/"><img src="/assets/images/icons/edit.png" alt="edit <?=$idea['name']?>" /></a></span>
@@ -13,8 +11,11 @@
 		<h4>Details</h4>
 		<p><?=$idea['details']?></p>
 		<?php endif?>
-		<h4>Try it</h4>
+		<h4>Instructions</h4>
 		<?=$idea['instructions']?>
+		<?php if ($idea['url']):?>
+		<div class="button try"><a href="<?=$idea['url']?>">Try it</a></div>
+		<?endif?>
 		<h4>Credits</h4>
 		<?=$idea['credits']?>
 	</div>
