4916d879204ea0be66c84ceaa60220d5d07b26a7
Ed Bassett <ed@devsketchpad.com>
2012-05-01 21:45:41 +1000

    added link to image

--- codeigniter/application/views/gallery_view.php
+++ codeigniter/application/views/gallery_view.php
@@ -4,7 +4,7 @@ foreach ($ideas as $idea):
 	$odd_row = !$odd_row;
 ?>
 <div class="page<?php echo $odd_row ? ' odd' : '' ?>">
-	<span class="thumbnail"><img src="/uploads/<?php echo $idea['image']; ?>" alt="project thumbnail" /></span>
+	<span class="thumbnail"><a href="/gallery/<?=$idea['slug']?>"><img src="/uploads/<?php echo $idea['image']; ?>" alt="project thumbnail" /></a></span>
 	<div class="details">
 		<h3><a href="/gallery/<?=$idea['slug']?>"><?=$idea['name']?></a></h3>
 		<p><?=$idea['description']?></p>
