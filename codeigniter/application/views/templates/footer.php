35cf91873a84e2589639d8123d4796fb1ef73d97
Ed Bassett <ed@devsketchpad.com>
2012-05-01 22:04:01 +1000

    expanded the footer in line with usability suggestions and divided it into sections for styling

--- codeigniter/application/views/templates/footer.php
+++ codeigniter/application/views/templates/footer.php
@@ -1,7 +1,15 @@
 </div>
 <div id="footer">
-<p>Copyright &copy; 2012 Ed Bassett</p>
-<p>Built on CodeIgniter</p>
+	<div class="section">
+		<p>Copyright &copy; 2012 Ed Bassett</p>
+		<p>Built on <a href="http://codeigniter.com/">CodeIgniter</a></p>
+	</div>
+	<div class="section">
+		<p><a href="/about/">About</a></p>
+	</div>
+	<div class="section">
+		<p><a href="/contact/">Contact</a></p>
+	</div>
 </div>
 </body>
 </html>
