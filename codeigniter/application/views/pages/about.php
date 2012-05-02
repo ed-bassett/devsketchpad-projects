74873e0d0df8b4327dbec3fa4b18ddf0a3acb67e
Ed Bassett <ed@devsketchpad.com>
2012-05-02 13:35:15 +1000

    expanded about page

--- codeigniter/application/views/pages/about.php
+++ codeigniter/application/views/pages/about.php
@@ -1,3 +1,5 @@
 <div class="page">
-<p>This is a sketchpad for my web development projects and game development ideas. They are all in various stages of completeness and will likely have bugs; the point is more to learn than to create perfection.</p>
+<h3>What</h3><p>This is a sketchpad for web development projects and game development ideas. They are all in various stages of completeness and will likely have bugs; the point is more to learn than to create perfection.</p>
+<h3>Who</h3><p>The bulk of these ideas are developed by <a href="/contact/">Ed Bassett</a>, however some are collaborations, whether that is in forming the concepts (<a href="https://plus.google.com/110984378052301505970/about">Vincent Tan</a>) or in coding (<a href="https://plus.google.com/109296784249030057702/about">Rob Norris</a>).</p>
+<h3>Why</h3><p>Ideas serve little purpose when concealed. This is simply a place where others can view or contribute to ideas that might otherwise be forgotten.</p>
 </div>
